b4fc04faf3967e67e3a061eab30317043682b726
Tallmann <Tm_gang@inbox.ru>
2025-08-13 13:38:27 +0300

    Поменял  логику ExplosionCube для реалистичного взрыва.

--- Assets/Scripts/ExplosionCube.cs
+++ Assets/Scripts/ExplosionCube.cs
@@ -1,24 +1,66 @@
+using NUnit.Framework.Constraints;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionCube : MonoBehaviour
-{
-    [SerializeField] private float _explosionForce;
-    [SerializeField] private float _explosionRadius;
-    [SerializeField] private float _upwardModifier;
+{    
+    [SerializeField] private float _baseForce = 100f;
+    [SerializeField] private float _baseRadius = 5f;
+    [SerializeField] private float _upwardModifier = 0.5f;
+    [SerializeField] private float _sizeForceMultiplier;
+    [SerializeField] private float _sizeRadiusMultiplier;
 
-    public void ApplyExplosion(Vector3 explosionCenter, List<Cube> spawnedObjects)
+    public void ApplyExplosion(Vector3 explosionCenter, List<Cube> specificCubes)
     {
-        float minvalue = 0.8f;
-        float maxValue = 1.2f;
+        List<Rigidbody> allAffectedRigidbodies = GetExplosionObjects(explosionCenter);
 
-        foreach (Cube spawnedObject in spawnedObjects)
+        foreach (Rigidbody rb in allAffectedRigidbodies)
         {
-            if (spawnedObject.Rigidbody != null)
+            ApplyExplosionToRigidbody(rb, explosionCenter);
+        }
+
+        if (specificCubes != null)
+        {
+            foreach (Cube cube in specificCubes)
             {
-                spawnedObject.Rigidbody.AddExplosionForce(_explosionForce * Random.Range(minvalue, maxValue),
-                explosionCenter, _explosionRadius, _upwardModifier, ForceMode.Impulse);
+                if (cube != null && cube.Rigidbody != null &&
+                    !allAffectedRigidbodies.Contains(cube.Rigidbody))
+                {
+                    ApplyExplosionToRigidbody(cube.Rigidbody, explosionCenter);
+                }
             }
         }
+    }
+
+    private void ApplyExplosionToRigidbody(Rigidbody rigidbody, Vector3 explosionCenter)
+    {
+        float startSize = 1f;
+        float distance = Vector3.Distance(explosionCenter, rigidbody.position);
+        float sizeFactor = startSize / rigidbody.transform.localScale.magnitude; 
+
+        float explosionForce = _baseForce * sizeFactor * _sizeForceMultiplier;
+        float explosionRadius = _baseRadius * sizeFactor * _sizeRadiusMultiplier;
+
+        float distanceFactor = Mathf.Clamp01(1 - (distance / explosionRadius));
+        explosionForce *= distanceFactor;
+
+        rigidbody.AddExplosionForce(explosionForce,explosionCenter,explosionRadius,
+            _upwardModifier,ForceMode.Impulse);
+    }
+
+    private List<Rigidbody> GetExplosionObjects(Vector3 explosionCenter)
+    {
+        Collider[] hits = Physics.OverlapSphere(explosionCenter, _baseRadius * _sizeRadiusMultiplier);
+        List<Rigidbody> rigidbodys = new List<Rigidbody>();
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.attachedRigidbody != null && !rigidbodys.Contains(hit.attachedRigidbody))
+            {
+                rigidbodys.Add(hit.attachedRigidbody);
+            }
+        }
+
+        return rigidbodys;
     }
 }
